afb016ff8e4e48e406227a145cc922d6e4e43370
Satheesha G R <122616518+satheesha-gr@users.noreply.github.com>
2023-07-20 19:30:27 +0530

    react handson3

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FormComponent from './Components/FormComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
